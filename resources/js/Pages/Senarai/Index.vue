12d6308a4dfad9becc500c2e25dcc96c9511081e
NBS_Rasyidi <rasyidialwee@gmail.com>
2023-05-03 11:34:20 +0800

    update

--- resources/js/Pages/Senarai/Index.vue
+++ resources/js/Pages/Senarai/Index.vue
@@ -14,6 +14,7 @@ defineProps({
             </h2>
         </template>
 
+        <a href="">Create New List</a>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
